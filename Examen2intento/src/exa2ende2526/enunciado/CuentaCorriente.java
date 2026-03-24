c2d8e7f5c929e2b60095468eb445e608234aa5c0
pabloveramsanjose <pabloveram.sanjose@fundacionloyola.net>
2026-03-24 20:11:57 +0100

    Commit de la rama creada de CuenraCorriente y su respectiva clase

---
+++ Examen2intento/src/exa2ende2526/enunciado/CuentaCorriente.java
@@ -0,0 +1,17 @@
+package exa2ende2526.enunciado;
+
+public class CuentaCorriente extends CuentaBancaria {
+	
+	private double comision;
+
+	public CuentaCorriente(String titular, double saldoInicial) {
+		super(titular, saldoInicial);
+		this.comision  = comision;
+	}
+	
+
+	public double calcularInteres() {
+		return comision;
+	}
+	
+}
