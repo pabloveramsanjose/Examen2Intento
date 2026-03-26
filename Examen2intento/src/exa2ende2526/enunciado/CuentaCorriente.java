67a8361800d32155a9606ceea19b61f3035ba021
pabloveramsanjose <pabloveram.sanjose@fundacionloyola.net>
2026-03-26 19:10:11 +0100

    Commit de la rama master de la clase CuentaBancaria la cual he creado los metodos que lanzan los errores

--- Examen2intento/src/exa2ende2526/enunciado/CuentaCorriente.java
+++ Examen2intento/src/exa2ende2526/enunciado/CuentaCorriente.java
@@ -14,6 +14,23 @@ public class CuentaCorriente extends CuentaBancaria {
 		return comision;
 	}
 
+	public void ingresar(double cantidad) {
+		if ( cantidad < 0) {
+			throw new IllegalArgumentException("La cantidad no puede ser menor que 0");
+		}
+		
+	}
 	
-	
+	public void reintegrar(double cantidad) {
+		
+		if ( cantidad < 0) {
+			throw new IllegalArgumentException("La cantida no puede ser negativa");
+			
+		}
+		
+		if ( cantidad > saldo) {
+			throw new IllegalArgumentException("LA cantidad no puede ser superior al saldo");
+		}
+		
+	}
 }
